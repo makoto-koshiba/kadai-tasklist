68d27d60e7e0eff7ff457a3b6ed56fb4eb70a110
makoto.koshiba <ma.koshi@kdk-nagano.co.jp>
2021-05-20 23:46:58 +0000

    CRUD messages3

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -15,4 +15,7 @@
         </tr>
     </table>
 {!! link_to_route('messages.edit', 'このタスクを編集', ['message' => $message->id], ['class' => 'btn btn-light']) !!}
+{!! Form::model($message, ['route' => ['messages.destroy', $message->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
 @endsection

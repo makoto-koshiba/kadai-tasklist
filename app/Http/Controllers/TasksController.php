c6a948916eec2857cd87d441110660221e61c368
makoto.koshiba <ma.koshi@kdk-nagano.co.jp>
2021-05-20 08:26:38 +0000

    CRUD messages2

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Task;
-class TasKsController extends Controller
+class TasksController extends Controller
 {
     /**
      * Display a listing of the resource.
